b0f84f0e1a7b759cfc25facfea143b1f0717a076
infreeJ <kasious97@naver.com>
2025-12-29 20:04:38 +0900

    1개의 상품을 주문 가능한 Order 비즈니스 로직 구현

--- src/main/java/com/example/demo/service/OrderService.java
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -1,8 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Item;
 import com.example.demo.domain.Member;
+import com.example.demo.domain.Order;
+import com.example.demo.domain.OrderItem;
 import com.example.demo.dto.request.OrderRequest;
+import com.example.demo.repository.ItemRepository;
 import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.OrderItemRepository;
 import com.example.demo.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,12 +18,31 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final ItemRepository itemRepository;
 
     public void order(OrderRequest orderRequest) {
 
         Member member = memberRepository.findById(orderRequest.getMemberId())
                 .orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
 
+        Item item = itemRepository.findById(orderRequest.getItemId())
+                .orElseThrow(() -> new IllegalArgumentException("상품 정보를 찾을 수 없습니다."));
 
+        Order order = Order.builder()
+                .member(member)
+                .build();
+
+        int orderPrice = orderRequest.getCount() * item.getPrice();
+
+        OrderItem orderItem = OrderItem.builder()
+                .order(order)
+                .item(item)
+                .orderPrice(orderPrice)
+                .count(orderRequest.getCount())
+                .build();
+
+        orderRepository.save(order);
+        orderItemRepository.save(orderItem);
     }
 }
