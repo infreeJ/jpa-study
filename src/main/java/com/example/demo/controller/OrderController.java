fe113de4019198d61c4542ea1d1ae563fad7d7ea
infreeJ <kasious97@naver.com>
2025-12-29 19:48:17 +0900

    주문 기능 구현 시작

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+
 }
