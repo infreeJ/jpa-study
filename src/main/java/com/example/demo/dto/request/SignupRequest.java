b77e0ae980414b2758629e468dd1d21c7e37bc72
infreeJ <kasious97@naver.com>
2025-12-29 16:53:54 +0900

    SignupRequest 정의

---
+++ src/main/java/com/example/demo/dto/request/SignupRequest.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class SignupRequest {
+
+    private String name;
+    private String street;
+    private String zipcode;
+}
