03f4ba0f510c1e651aab00a2e414f4a5957e46a3
infreeJ <47980528+infreeJ@users.noreply.github.com>
2025-12-29 21:07:47 +0900

    주문 기능 OrderItem 영속성 전이 설계 완료 (#4)
    * 영속성 전이를 위한 엔티티 설계 개선
    
    * addOrderItem
    
    * 주문 기능 OrderItem 영속성 전이 설계 완료

--- src/main/java/com/example/demo/domain/Order.java
+++ src/main/java/com/example/demo/domain/Order.java
@@ -6,6 +6,8 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -24,6 +26,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     @Column
     private int totalPrice;
 
@@ -35,4 +41,11 @@ public class Order {
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private Status status = Status.PAYMENT;
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+
 }
