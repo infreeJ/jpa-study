d62087ee728373cd7b6e6f6a91595eda4b806374
infreeJ <47980528+infreeJ@users.noreply.github.com>
2025-12-29 19:44:54 +0900

    Item 추가 기능 구현
    * 상품 추가 구현 시작
    
    * Item toEntity() 구현
    
    * Item 추가 기능 구현 완료

--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,4 +1,20 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Item;
+import com.example.demo.dto.request.ItemRegistrationRequest;
+import com.example.demo.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public void Registration(ItemRegistrationRequest itemRegistrationRequest) {
+
+        Item item = itemRegistrationRequest.toEntity();
+        itemRepository.save(item);
+    }
 }
